2d587072d97a5a4dfac290f0e17b7f977e56719f
arladiy96-commits <arladiy96@gmail.com>
2026-09-24 14:40:02 +0500

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,1 +1,16 @@
+plugins {
+    id("com.android.application")
+}
 
+android {
+    namespace = "com.arladiy.glassgoldicons"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "com.arladiy.glassgoldicons"
+        minSdk = 26
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.0"
+    }
+}
