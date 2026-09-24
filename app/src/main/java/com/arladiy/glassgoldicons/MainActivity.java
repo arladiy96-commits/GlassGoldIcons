f03d8961bffb82964e566446eb9b11ad12df2b55
arladiy96-commits <arladiy96@gmail.com>
2026-09-24 14:41:13 +0500

    Add files via upload

--- app/src/main/java/com/arladiy/glassgoldicons/MainActivity.java
+++ app/src/main/java/com/arladiy/glassgoldicons/MainActivity.java
@@ -1,1 +1,18 @@
+package com.arladiy.glassgoldicons;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        TextView view = new TextView(this);
+        view.setText("Glass Gold Icons\n\nHyperIcons test pack\n\nTelegram\nWhatsApp\nYouTube\nChatGPT");
+        view.setTextSize(20);
+        view.setPadding(48, 96, 48, 48);
+        setContentView(view);
+    }
+}
